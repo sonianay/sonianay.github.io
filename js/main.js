d974e0cf0eed828eba8dcbe4158bd473b9251239
Anay Soni <anay_soni@hotmail.com>
2026-01-11 17:36:47 -0500

    Removed CSS & HTML clutter, updated commands, removed txt file clutter

--- js/main.js
+++ js/main.js
@@ -1,36 +1,24 @@
 const trolls = [
-  "nah",
-  "but thats boooring",
   "nah im good",
   "but i'm tiiired",
-  "nope",
   "are you serious?",
-  "what for?",
   "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
 ];
 
-// function troll() {
-//   // this.echo(trolls[Math.floor(Math.random() * trolls.length)] + "\n");
-// }
-
 var term = {
   top: function () {
-    var intro =
-      "C:\\Users\\Guest>type about.txt \n" +
-      " \n" +
-      "Thanks to Jakub T. Jankiewicz for the base library. \n" +
-      "https://terminal.jcubic.pl/#download for the download. \n" +
-      "https://github.com/sponsors/jcubic for donations to Jankiewicz.\n\n" +
-      "COMMANDS : \n" +
-      "    - &#39;help&#39; \n" +
-      "    - &#39;dir&#39; \n" +
-      "    - &#39;type about.txt&#39; \n" +
-      "    - &#39;type experience.txt&#39; \n";
-
+    var intro = "Guest@TotallyRealLinux:~$ display about\n\n" +
+                "COMMANDS TO TRY:\n" +
+                "display about\n" + 
+                "display work\n" + 
+                "help\n\n" + 
+                "Thanks to Jakub T. Jankiewicz for the base library.\n" +
+                "https://github.com/sponsors/jcubic for donations.\n\n";
     return intro;
   },
-  // ngl got no clue how to match keys to same val
-  test: function() {this.echo("trollls")},
+  test: function() {
+    this.echo("trollls")
+  },
   ls: function () {
     this.echo(trolls[Math.floor(Math.random() * trolls.length)] + "\n");
   },
@@ -65,43 +53,34 @@ var term = {
     this.echo(trolls[Math.floor(Math.random() * trolls.length)] + "\n");
   },
   help: function () {
-    fetch("../assets/help.txt")
-      .then((response) => response.text())
-      .then((text) => this.echo(text));
+    this.echo(trolls[Math.floor(Math.random() * trolls.length)] + "\n");
   },
-  dir: function () {
-    fetch("../assets/ls.txt")
-      .then((response) => response.text())
-      .then((text) => this.echo(text));
+  list: function () {
+    this.echo(trolls[Math.floor(Math.random() * trolls.length)] + "\n");
   },
-  type: function (file) {
-    if (file == "about.txt") {
+  display: function (file) {
+    if (file == "about") {
       fetch("../assets/about.txt")
         .then((response) => response.text())
         .then((text) => this.echo(text))
         .catch((error) => {
           console.error("err", error);
         });
-    } else if (file == "experience.txt") {
-      fetch("../assets/experience.txt")
-        .then((response) => response.text())
-        .then((text) => this.echo(text));
+    } else if (file == "work") {
+      window.open("/assets/soni_anay_resume.pdf");
     } else {
-      this.echo("The system cannot find the file specified. \n");
+      this.echo("No file with that name \n");
     }
-  },
-  start: function (link) {
-    window.open(link);
   },
 };
 
 jQuery(document).ready(function ($) {
-  $(".test").terminal(term, {
+  $(".terminal-container").terminal(term, {
     greetings: function (cb) {
       cb(term.top());
     },
     completion: true,
     checkArity: false,
-    prompt: "C:\\Users\\Guest>",
+    prompt: "Guest@TotallyRealLinux:~$ ",
   });
 });
